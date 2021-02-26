1f94a1c37ece1422b7d381af76d6f3164976aa34
zhanzhao <hanzhanzhao@wellekq.com>
2021-02-26 15:03:59 +0800

    upload

---
+++ 14/14.6.c
@@ -0,0 +1,29 @@
+//
+// Created by zhanzhao on 2021/2/26.
+//
+
+#include <stdio.h>
+#define FUNDLEN 50
+
+struct funds {
+    char bank[FUNDLEN];
+    double bankfund;
+    char save[FUNDLEN];
+    double savefund;
+};
+double sum(const struct funds *);
+int main(void)
+{
+    struct funds stan = {
+            "Garlic-Melon Bank",
+            4032.27,
+            "Lucky's Saving and Loan",
+            8543.95
+    };
+    printf("Stan has a total of $%.2f.\n",sum(&stan));
+    return 0;
+}
+double sum(const struct funds * money)
+{
+    return money->savefund+money->bankfund;
+}
